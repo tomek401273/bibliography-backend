83d2456ee8292cf4af581844001bf30db2dd2e29
tomek401273 <tomek371240@gmail.com>
2019-05-18 19:22:20 +0200

    docker-compose

---
+++ jenkins/jenkins.groovy
@@ -0,0 +1,14 @@
+node ('master') {
+    stage('pull') {
+        git 'https://github.com/tomek401273/bibliography-backend.git'
+    }
+    stage('package-bib') {
+        sh script 'mvn package'
+    }
+    stage('docker') {
+        sh script 'docker build -t tomek371240/bibl:6 .'
+    }
+    stage('docker-deploy') {
+        sh script 'docker stack deploy -c biblio.yml biblio-stack'
+    }
+}
