5659bc6832bc97d034c133bf8b14dc23b6fac468
tomek401273 <tomek371240@gmail.com>
2019-05-22 16:27:40 +0200

    database-connections

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -3,12 +3,12 @@ node ('master') {
         git 'https://github.com/tomek401273/bibliography-backend.git'
     }
     stage('package-bib') {
-        sh script 'mvn package'
+        sh script: 'mvn package'
     }
     stage('docker') {
-        sh script 'docker build -t tomek371240/bibl:6 .'
+        sh script: 'docker build -t tomek371240/bibl:2.1 .'
     }
     stage('docker-deploy') {
-        sh script 'docker stack deploy -c biblio.yml biblio-stack'
+        sh script: 'docker stack deploy -c biblio.yml biblio-stack'
     }
 }
