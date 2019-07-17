c0168ec8bff43d8b0f4ec81a773e0c96aa7992ed
tomek401273 <tomek371240@gmail.com>
2019-07-17 15:13:29 +0200

    cleaning

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,10 +2,6 @@ node ('master') {
     stage('pull') {
         git 'https://github.com/tomek401273/bibliography-backend.git'
     }
-    stage('package-bib') {
-        sh script: 'mvn clean'
-        sh script: 'mvn package'
-    }
     stage('docker') {
         sh script: 'docker build -t tomek371240/bibl:2.2 .'
     }
