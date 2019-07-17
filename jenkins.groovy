e73154fac58243f41cdc55354a95280736404ff7
tomek401273 <tomek371240@gmail.com>
2019-07-17 13:44:55 +0200

    jenkins

---
+++ jenkins.groovy
@@ -0,0 +1,15 @@
+node ('master') {
+    stage('pull') {
+        git 'https://github.com/tomek401273/bibliography-backend.git'
+    }
+    stage('package-bib') {
+        sh script: 'mvn clean'
+        sh script: 'mvn package'
+    }
+    stage('docker') {
+        sh script: 'docker build -t tomek371240/bibl:2.2 .'
+    }
+    stage('docker-deploy') {
+        sh script: 'docker stack deploy -c biblio.yml biblio-stack'
+    }
+}
